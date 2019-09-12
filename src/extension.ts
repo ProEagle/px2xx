816530a6fa7312227481359c747967e01ef7ee9e
13203938325 <lgz838453239@163.com>
2019-09-12 22:36:54 +0800

    更改描述文件，删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -27,33 +27,6 @@ export function activate(context: vscode.ExtensionContext) {
 		let providerDisposable = vscode.languages.registerCompletionItemProvider(lan, provider);
 		context.subscriptions.push(providerDisposable);
 	}
-
-	// const process = new vscode.ProcessExecution(config);
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerTextEditorCommand('extension.px2xx', (textEditor: vscode.TextEditor, edit) => {
-	// 	// The code you place here will be executed every time your command is executed
-
-	// 	// Display a message box to the user
-	// 	vscode.window.showWarningMessage('px2upx vs code!');
-
-	// 	const doc = textEditor.document;
-	// 	let selection: vscode.Selection | vscode.Range = textEditor.selection;
-
-	// 	if(selection.isEmpty) {
-	// 		const start = new vscode.Position(0, 0);
-	// 		const end = new vscode.Position(doc.lineCount - 1, doc.lineAt(doc.lineCount - 1).text.length);
-	// 		selection = new vscode.Range(start, end);
-	// 	}
-
-	// 	let text = doc.getText(selection);
-	// 	textEditor.edit(builder => {
-	// 		builder.replace(selection, process.convertAll(text));
-	// 	});
-	// });
-	
-	// context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
